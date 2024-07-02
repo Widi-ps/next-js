d483d19c96817a866617090297a451b7f4dae028
widi-ps <widyapujisaputro@gmail.com>
2024-07-03 01:08:21 +0700

    add user button component

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import styles from './postPage.module.css'
+import styles from './postPage.module.css';
+import ViewUserButton from '../components/ViewUserButton';
 
 const Posts = () => {
-  return <h1 className={styles.bgRed}>POSTINGAN PAGE</h1>;
+  return (
+    <>
+      <h1 className={styles.bgRed}>POSTINGAN PAGE</h1>
+      <ViewUserButton />
+    </>
+  );
 };
 
 export default Posts;
