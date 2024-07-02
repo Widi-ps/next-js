1880823ca269b8b3de8168ab015e519c0c97fd68
widi-ps <widyapujisaputro@gmail.com>
2024-07-02 20:40:51 +0700

    import module css

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './postPage.module.css'
 
 const Posts = () => {
-  return <h1>POSTINGAN PAGE</h1>;
+  return <h1 className={styles.bgRed}>POSTINGAN PAGE</h1>;
 };
 
 export default Posts;
