f7564d24dc3de0a93c8aad2381614e9a9d1efaef
widi-ps <widyapujisaputro@gmail.com>
2024-07-03 09:41:20 +0700

    add Card List component

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './postPage.module.css';
-import ViewUserButton from '../components/ViewUserButton';
+import CardList from '../components/Posts/CardList';
 
-const Posts = () => {
+const base_url = 'https://jsonplaceholder.typicode.com/posts';
+
+const Posts = async () => {
+  const response = await fetch(base_url);
+  const posts = await response.json();
   return (
     <>
       <h1 className={styles.bgRed}>POSTINGAN PAGE</h1>
-      <ViewUserButton />
+      <p>{posts[0].body}</p>
+      <CardList>
+        <p>Hello</p>
+        <i>HI !!</i>
+      </CardList>
     </>
   );
 };
